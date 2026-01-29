c4b030431df853eb325a99302374bc409bd6fde8
VictorGabrielPA <0001151656@senaimgaluno.com.br>
2026-01-28 21:05:33 -0300

    segundo comit

--- script.js
+++
@@ -1,9 +0,0 @@
-function clicou() {
-    var email = document.getElementById("email").value
-    var password = document.getElementById("password").value
-
-
-
-        window.location.href = "pages/home.html"
-    
-}
